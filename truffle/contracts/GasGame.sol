060c5d16fc96713aba3b0de3474800fdb9da0a49
Maximiliann <maximilian.niemzik@maibornwolff.de>
2019-01-21 22:01:31 +0100

    Improve contract

--- truffle/contracts/GasGame.sol
+++ truffle/contracts/GasGame.sol
@@ -1,38 +1,62 @@
-pragma solidity ^0.5;
+pragma solidity ^0.5.2;
+
+import 'openzeppelin-solidity/blob/master/contracts/ownership/Ownable.sol';
 
 contract Scoreboard {
     function addScore(address, uint256) public;
+    function isPlayer(address) public view returns (bool);
+    function addPlayer(address) public;
 }
 
-contract GasGame {
+contract GasGame is Ownable {
 
     Scoreboard scoreboard;
 
-    uint256 txValue = 3000000;
+    uint256 txValue;
 
     uint256 lastBlock;
 
     constructor(address _scoreboard) public {
+        // Sets the address of the highscore
         scoreboard = Scoreboard(_scoreboard);
+        // Sets the lastBlock to a round number, so that the game takes place every thousandth block
+        lastBlock = block.number - (block.number % 1000);
+        txValue = 3000000;
     }
 
     event scoreAdded(address player, uint256 score);
 
-    function play() public payable {
+    modifier onlyThousandthBlock(){
+      require(block.number % 1000 == 0, 'This is not a thousandth block.');
+      _;
+    }
+
+    modifier onlyCorrectValue(){
         uint256 leftover = gasleft();
-        uint256 used = 21374;
+        uint256 used = 21430;
         uint256 totalGas = leftover + used;
         uint256 weiPayed = tx.gasprice * totalGas;
         uint256 totalWei = weiPayed + msg.value;
+        emit scoreAdded(msg.sender, leftover);
         require(totalWei == txValue);
-        uint256 score = leftover;
+        _;
+    }
+
+    function play() public payable onlyThousandthBlock {
+        uint256 score = gasleft();
         if(lastBlock != block.number){
             //You are the first in the block
             lastBlock = block.number;
             score = score * 2;
         }
+        if(!scoreboard.isPlayer(msg.sender)){
+            scoreboard.addPlayer(msg.sender);
+        }
         scoreboard.addScore(msg.sender, score);
         emit scoreAdded(msg.sender, score);
     }
 
+    function setTxValue(uint256 _newValue) public onlyOwner {
+        txValue = _newValue;
+    }
 }
