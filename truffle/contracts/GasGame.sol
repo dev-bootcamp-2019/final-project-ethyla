f70c9929e854a0396975b35e624e001dd0f684ee
Maximiliann <maximilian.niemzik@maibornwolff.de>
2019-01-26 16:17:25 +0100

    Add natspec comments

--- truffle/contracts/GasGame.sol
+++ truffle/contracts/GasGame.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.5;
 
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
+// Interface for calling the permanent storage
 contract Scoreboard {
     function addScore(address, uint256) public;
     function isPlayer(address) public view returns (bool);
@@ -11,31 +12,41 @@ contract Scoreboard {
     function getScore(address) public view returns (uint256);
 }
 
+/// @title A Contract that enforces same total value for all tx
 contract GasGame is Ownable {
 
+    // Permanent Scoreboard
     Scoreboard scoreboard;
 
+    // Enforced exact total value a tx must have
     uint256 txValue;
 
+    // Last block this game was played at
     uint256 lastBlock;
 
+    // Player played the game and got points, score is points of current round
     event ScoreAdded(address player, uint256 score);
 
     constructor(address _scoreboard) public {
         // Sets the address of the highscore
         scoreboard = Scoreboard(_scoreboard);
-        // Sets the lastBlock to a round number, so that the game takes place every thousandth block
+        // Sets the lastBlock to a round number, so that the game takes place every thousandth block (Only to make the number nice :))
         lastBlock = block.number - (block.number % 1000);
+        // Sets the enforced total value of a tx
         txValue = 3000000;
     }
 
+    // Only allows calls on blocknumbers ending with 000
     modifier onlyThousandthBlock() {
         require(block.number % 1000 == 0, "This is not a thousandth block.");
         _;
     }
 
+    // Enforces the exact total amount of a tx, by computing total gaslimit and used wei for purchasing gas plus remaining wei value of the tx
+    // Weakness to this aproach: The use can set a huge gaslimit which never gets consumed and so doesn't lose as much money, but they would get lower points
     modifier onlyCorrectValue() {
         uint256 leftover = gasleft();
+        // Gas used up to this point is always 21430 if called from an externally owned account, so no contract calls for now
         uint256 used = 21430;
         uint256 totalGas = leftover + used;
         uint256 weiPayed = tx.gasprice * totalGas;
@@ -44,27 +55,37 @@ contract GasGame is Ownable {
         _;
     }
 
+    /// @dev Function that calculates total points for a player and saves them to the scoreboard
     function play() public payable onlyThousandthBlock {
         uint256 score = gasleft();
+        // If last block is still the block of the last game the caller is the first one this block
         if (lastBlock != block.number) {
-            //You are the first in the block
+            //Set a new lastBlock so that following tx know that they aren't the first
             lastBlock = block.number;
+            // Double the score as a reward for being the first
             score = score * 2;
         }
+        // Check if user is already on scoreboard
         if (!scoreboard.isPlayer(msg.sender)) {
             scoreboard.addPlayer(msg.sender);
         }
+        // Add score to scoreboard
         scoreboard.addScore(msg.sender, score);
         emit ScoreAdded(msg.sender, score);
     }
 
+    /// @param _newValue of the total allowed value of a single tx
+    /// @dev Sets a new value that tx must use
     function setTxValue(uint256 _newValue) public onlyOwner {
         txValue = _newValue;
     }
 
+    /// @dev Convenience function to return all addresses of all players
+    /// @return an array containing all registered players
     function getAllPlayers() public view returns (address[]) {
         uint256 highestIndex = scoreboard.playerCount();
         address[] allPlayers;
+        // Loop through the "array"
         for (uint256 i = 0; i <= highestIndex; i++) {
             address player = scoreboard.getPlayerById(i);
             if (scoreboard.isPlayer(player)) {
@@ -73,6 +94,9 @@ contract GasGame is Ownable {
         }
     }
 
+    /// @param _player address of the player
+    /// @dev Convenience function to return the total score of a single player
+    /// @return the total number of points of the player
     function getScore(address _player) public view returns (uint256) {
         return scoreboard.getScore(_player);
     }
