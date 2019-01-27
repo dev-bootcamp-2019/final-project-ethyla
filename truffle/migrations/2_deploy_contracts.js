e91fdc1aa055cfa9d33767b3678ef5dbdfdf40a4
Maximiliann <maximilian.niemzik@maibornwolff.de>
2019-01-27 13:30:59 +0100

    Configure truffle

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@ const VyperStorage = artifacts.require('VyperStorage');
 const GasGame = artifacts.require('GasGame');
 
 module.exports = function (deployer) {
-  deployer.deploy(VyperStorage);
-  then(() => deployer.deploy(GasGame, VyperStorage.address));
+  deployer.deploy(VyperStorage)
+    .then(() => deployer.deploy(GasGame, VyperStorage.address));
 };
--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -1,32 +1,19 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura API
- * keys are available for free at: infura.io/register
- *
- *   > > Using Truffle V5 or later? Make sure you install the `web3-one` version.
- *
- *   > > $ npm install truffle-hdwallet-provider@web3-one
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const privKeyrinkeby = '0xd9b4fd1876c2a41eeddde58109ee76551454a3d9a1e9a9db227eefb62a632b86';
+const PrivateKeyProvider = require('truffle-privatekey-provider');
 
 module.exports = {
-}
+
+  development: {
+    host: 'localhost',
+    port: 8545,
+    network_id: '*', // Match any network id
+  },
+  rinkeby: {
+    host: 'localhost', // Connect to geth on the specified
+    port: 8545,
+    provider: () => new PrivateKeyProvider(privKeyrinkeby, 'https://rinkeby.infura.io/v3/'),
+    from: '0x3feAC4ECfBfcB441cf91552e00445d1c807D9D62',
+    network_id: 4,
+    gas: 4612388, // Gas limit used for deploys
+  },
+};
