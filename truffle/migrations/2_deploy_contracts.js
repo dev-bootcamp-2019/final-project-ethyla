fcbd622de899a7efa65cfc488a10c92a2051d6cd
Maximiliann <maximilian.niemzik@maibornwolff.de>
2019-01-27 17:09:33 +0100

    Rewrite truffle config with async await

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -1,7 +1,8 @@
 const VyperStorage = artifacts.require('VyperStorage');
 const GasGame = artifacts.require('GasGame');
 
-module.exports = function (deployer) {
-  deployer.deploy(VyperStorage)
-    .then(() => deployer.deploy(GasGame, VyperStorage.address));
+module.exports = async function (deployer) {
+  await deployer.deploy(VyperStorage);
+
+  await deployer.deploy(GasGame, VyperStorage.address);
 };
