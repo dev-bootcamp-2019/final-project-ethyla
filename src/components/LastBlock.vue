16bded3d5cec17634507746d0bc289f9f0521e72
Maximiliann <maximilian.niemzik@maibornwolff.de>
2019-01-27 12:37:55 +0100

    Move lastblock to own component

---
+++ src/components/LastBlock.vue
@@ -0,0 +1,124 @@
+<template>
+<v-card>
+  <v-card-title>
+    <div class="headline">Last Round</div>
+    <v-spacer></v-spacer>
+    <v-text-field v-model="search" label="Search" single-line hide-details></v-text-field>
+  </v-card-title>
+  <v-data-table hide-actions :pagination.sync="pagination" :headers="headers" :items="tx" :search="search" class="elevation-1 scroll" style="height:33vh;">
+    <template slot="headerCell" slot-scope="props">
+        <v-tooltip bottom>
+          <span slot="activator">
+            {{ props.header.text }}
+          </span>
+          <span>
+            {{ props.header.tooltip }}
+          </span>
+        </v-tooltip>
+      </template>
+    <template slot="items" slot-scope="props">
+          <td class="text-xs-left ">{{ props.item.points }}</td>
+          <td class="text-xs-left">{{ props.item.address }}</td>
+          <td class="text-xs-left">{{ props.item.gasPrice }}</td>
+          <td class="text-xs-left">{{ props.item.gasLimit }}</td>
+
+        </template>
+    <v-alert slot="no-results" :value="true" color="error">
+      Your search for "{{ search }}" found no results.
+    </v-alert>
+
+  </v-data-table>
+</v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      search: '',
+      pagination: {
+        sortBy: 'points',
+        descending: true,
+        rowsPerPage: -1,
+      },
+      headers: [{
+        text: 'Total Points',
+        tooltip: 'Gathered points since the beginning.',
+        align: 'left',
+        sortable: true,
+        value: 'points',
+      }, {
+        text: 'Address',
+        tooltip: 'Address of the player.',
+        align: 'left',
+        sortable: false,
+        value: 'address',
+      }, {
+        text: 'GasPrice',
+        tooltip: 'GasPrice of the transaction.',
+        align: 'left',
+        sortable: true,
+        value: 'gasPrice',
+      }, {
+        text: 'GasLimit',
+        tooltip: 'GasLimit of the transaction.',
+        align: 'left',
+        sortable: true,
+        value: 'gasLimit',
+      }],
+      tx: [{
+        value: false,
+        points: 101,
+        address: '0x98765678...',
+        gasLimit: '1230000',
+        gasPrice: '1000000000',
+      }, {
+        value: false,
+        points: 101,
+        address: '0x98765678...',
+        gasLimit: '1230000',
+        gasPrice: '100000000',
+      }, {
+        value: false,
+        points: 101,
+        address: '0x98765678...',
+        gasLimit: '1230000',
+        gasPrice: '10000000',
+      }, {
+        value: false,
+        points: 101,
+        address: '0x98765678...',
+        gasLimit: '1230000',
+        gasPrice: '2000000000',
+      }, {
+        value: false,
+        points: 101,
+        address: '0x98765678...',
+        gasLimit: '1230000',
+        gasPrice: '1000000000',
+      }, {
+        value: false,
+        points: 101,
+        address: '0x98765678...',
+        gasLimit: '1230000',
+        gasPrice: '1000000000',
+      }, {
+        value: false,
+        points: 101,
+        address: '0x98765678...',
+        gasLimit: '1230000',
+        gasPrice: '1000000000',
+      }, {
+        value: false,
+        points: 101,
+        address: '0x98765678...',
+        gasLimit: '1230000',
+        gasPrice: '1000000000',
+      }],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,62 +6,7 @@
         <TxConfigurator></TxConfigurator>
       </v-flex>
       <v-flex style="min-height:50%; max-height:50%;">
-        <!-- Current Block to see pending tx -->
-        <v-card v-if="nowPlaying">
-          <v-card-title>
-            Current Block
-            <v-spacer></v-spacer>
-            <v-text-field v-model="search" label="Search" single-line hide-details></v-text-field>
-          </v-card-title>
-          <v-data-table hide-actions :pagination.sync="pagination" :headers="headers" :items="transactions" :search="search" class="elevation-1 scroll" style="max-height:33vh;">
-            <template slot="headerCell" slot-scope="props">
-              <v-tooltip bottom>
-                <span slot="activator">
-                  {{ props.header.text }}
-                </span>
-                <span>
-                  {{ props.header.tooltip }}
-                </span>
-              </v-tooltip>
-            </template>
-            <template slot="items" slot-scope="props" >
-              <td class="text-xs-left ">{{ props.item.points }}</td>
-              <td class="text-xs-left">{{ props.item.address }}</td>
-            </template>
-            <v-alert slot="no-results" :value="true" color="error">
-              Your search for "{{ search }}" found no results.
-            </v-alert>
-          </v-data-table>
-        </v-card>
-
-        <!-- Most recent Block wherer a game happend -->
-        <v-card v-else>
-          <v-card-title>
-            Current Block
-            <v-spacer></v-spacer>
-            <v-text-field v-model="search" label="Search" single-line hide-details></v-text-field>
-          </v-card-title>
-          <v-data-table hide-actions :pagination.sync="pagination" :headers="headers" :items="players" :search="search" class="elevation-1 scroll" style="height:33vh;">
-            <template slot="headerCell" slot-scope="props">
-              <v-tooltip bottom>
-                <span slot="activator">
-                  {{ props.header.text }}
-                </span>
-                <span>
-                  {{ props.header.tooltip }}
-                </span>
-              </v-tooltip>
-            </template>
-            <template slot="items" slot-scope="props">
-                <td class="text-xs-left ">{{ props.item.points }}</td>
-                <td class="text-xs-left">{{ props.item.address }}</td>
-              </template>
-            <v-alert slot="no-results" :value="true" color="error">
-              Your search for "{{ search }}" found no results.
-            </v-alert>
-
-          </v-data-table>
-        </v-card>
+        <LastBlock></LastBlock>
       </v-flex>
     </v-layout>
   </v-flex>
@@ -74,185 +19,18 @@
 <script>
 import TxConfigurator from '../components/TxConfigurator.vue';
 import Highscore from '../components/Highscore.vue';
+import LastBlock from '../components/LastBlock.vue';
 
 export default {
   name: 'home',
   components: {
     TxConfigurator,
     Highscore,
+    LastBlock,
   },
   data() {
     return {
-      search: '',
       nowPlaying: false,
-      transactions: [{
-        value: false,
-        points: 101,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 100,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 100,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 100,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 100,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 20,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 100,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }],
-      pagination: {
-        sortBy: 'points',
-        descending: true,
-        rowsPerPage: -1,
-      },
-      headers: [{
-        text: 'Total Points',
-        tooltip: 'Gathered points since the beginning.',
-        align: 'left',
-        sortable: true,
-        value: 'points',
-      }, {
-        text: 'Address',
-        tooltip: 'Address of the player.',
-        align: 'left',
-        sortable: false,
-        value: 'address',
-      }],
-      players: [{
-        value: false,
-        points: 101,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 100,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 100,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 100,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 100,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 20,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 100,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }, {
-        value: false,
-        points: 200,
-        address: '0x98765678...',
-      }],
     };
   },
 };
